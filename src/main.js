5854200554dc289900b0a55cd5f15226b5eec102
ziippy <ziippy@naver.com>
2022-07-28 18:51:34 +0900

    주석처리

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import VueRouter from 'vue-router'
 
-Vue.config.productionTip = false
+//Vue.config.productionTip = false
 
 Vue.use(VueRouter)
 
