83cb82c6c971b15541a43a1bdb491757c0579796
ziippy <ziippy@naver.com>
2022-07-30 19:18:53 +0900

    동적 라우트 작업중

---
+++ src/components/ContactByNo.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div>
+    <h1>연락처 상세</h1>
+    <div>
+      <table class="detail table table-bordered">
+        <tbody>
+            <tr class="active">
+              <td>일련번호</td>
+              <td>{{ contact.no }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
